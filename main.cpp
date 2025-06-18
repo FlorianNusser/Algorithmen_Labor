21cebb5cb3757e9fbbc18686e70401d57152af24
FlorianNusser <flori.nusser@gmail.com>
2025-06-18 14:34:50 +0200

    Main noch mal erstellen

--- main.cpp
+++ main.cpp
@@ -1,4 +1,20 @@
 #include <iostream>
 
-#include "Element.cpp"
-#include ""
+#include "Element.hpp"
+#include "Knoten.hpp"
+#include "LinkedQueue.hpp"
+#include "LinkedStack.hpp"
+#include "LinkedTree.hpp"
+#include "Menge.hpp"
+#include "Queue.hpp"
+#include "SearchTree.hpp"
+#include "Stack.hpp"
+#include "Tree.hpp"
+#include "TreeTools.hpp"
+#include "TreeToolsTest.hpp"
+
+int main(){
+
+
+    return 0;
+}
